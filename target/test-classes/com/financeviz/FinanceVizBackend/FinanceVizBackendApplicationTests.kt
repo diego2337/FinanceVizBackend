57f81b1273d81af8226b52a377328f50dd3abaf4
diego2337 <diegosc2337@gmail.com>
2024-12-22 15:37:44 -0300

    feat: create initial Kotlin Spring Boot project

---
+++ target/test-classes/com/financeviz/FinanceVizBackend/FinanceVizBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.financeviz.FinanceVizBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceVizBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
