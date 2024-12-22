a2f3843a917a3fdbc8a6f7eb65e9804a2b5eb992
diego2337 <diegosc2337@gmail.com>
2024-12-22 16:03:49 -0300

    feat: add FirebaseConfig to application

---
+++ src/main/kotlin/com/financeviz/FinanceVizBackend/infrastructure/framework/FirebaseConfig.kt
@@ -0,0 +1,31 @@
+package com.financeviz.FinanceVizBackend.infrastructure.framework
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.auth.FirebaseAuth
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.io.FileInputStream
+
+@Configuration
+class FirebaseConfig {
+
+    @Value("\${firebase.service-account-path}")
+    private lateinit var serviceAccountPath: String
+
+    @Bean
+    fun firebaseAuth(): FirebaseAuth {
+        val serviceAccount = FileInputStream(serviceAccountPath)
+        val options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build()
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options)
+        }
+
+        return FirebaseAuth.getInstance()
+    }
+}
